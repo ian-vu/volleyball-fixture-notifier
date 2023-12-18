e3dfdfe6c897eb8e1353f0de491132824dde620d
Ian Vu <iaanvuu@gmail.com>
2023-12-18 11:34:38 +1100

    Remove cdk

--- cdk/notifier/src/handler.py
+++
@@ -1,8 +0,0 @@
-from src.MessageGenerator import MessageGenerator
-
-
-def handler(event, context) -> None:
-    message = MessageGenerator().generate_message('toes')
-    print(message)
-
-    return message

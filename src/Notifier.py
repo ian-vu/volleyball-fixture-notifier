e7b1b9ed263cdc2c5a56dd2755a7f6f5873a32e7
Ian Vu <iaanvuu@gmail.com>
2023-12-18 17:29:58 +1100

    Add SNS topic for lambda to publish to

---
+++ src/Notifier.py
@@ -0,0 +1,13 @@
+import boto3
+
+
+class Notifier:
+    def __init__(self, sns_client: boto3.client = None) -> None:
+        self.sns_client = sns_client or boto3.client('sns')
+
+    def publish(self, topic: str, message: str):
+        self.sns_client.publish(
+            TopicArn=topic,
+            Subject="Volleyball Fixture Notifier",
+            Message=message
+        )
--- src/infra/aws/lambda_handler.py
+++ src/infra/aws/lambda_handler.py
@@ -1,12 +1,20 @@
+import os
+
+import boto3
 from aws_lambda_typing.context import Context
 from aws_lambda_typing.events import SNSEvent
 
 from src.MessageGenerator import MessageGenerator
+from src.Notifier import Notifier
 
 TEAM_NAME = "TICKLE"
 
+notifier = Notifier(sns_client=boto3.client('sns'))
+message_generator = MessageGenerator()
+
 
 def run(event: SNSEvent, context: Context) -> None:
-    message = MessageGenerator().generate_message(team_name=TEAM_NAME)
+    message = message_generator.generate_message(team_name=TEAM_NAME)
+    notifier.publish(topic=os.environ['SNS_EMAIL_TOPIC'], message=message)
 
     print(message)

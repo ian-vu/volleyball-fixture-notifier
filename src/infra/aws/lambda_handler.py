1fddcbc9778a58dd42e45f754c0fb593c0a6842d
Ian Vu <iaanvuu@gmail.com>
2023-12-18 13:11:47 +1100

    Update working lambda running Message Generator

--- src/infra/aws/lambda_handler.py
+++ src/infra/aws/lambda_handler.py
@@ -1,6 +1,12 @@
 from aws_lambda_typing.context import Context
 from aws_lambda_typing.events import SNSEvent
 
+from src.MessageGenerator import MessageGenerator
+
+TEAM_NAME = "TICKLE"
+
 
 def run(event: SNSEvent, context: Context) -> None:
-    print("hello world")
+    message = MessageGenerator().generate_message(team_name=TEAM_NAME)
+
+    print(message)
